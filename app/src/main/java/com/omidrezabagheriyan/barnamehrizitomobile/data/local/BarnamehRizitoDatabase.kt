ddd9d94634005fb72c4e67a00937c0dd082fc6b5
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-04-01 09:39:06 +0330

    Add and config database

--- app/src/main/java/com/omidrezabagheriyan/barnamehrizitomobile/data/local/BarnamehRizitoDatabase.kt
+++ app/src/main/java/com/omidrezabagheriyan/barnamehrizitomobile/data/local/BarnamehRizitoDatabase.kt
@@ -1,9 +1,10 @@
 package com.omidrezabagheriyan.barnamehrizitomobile.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.omidrezabagheriyan.barnamehrizitomobile.domain.models.Task
 
 @Database(entities = [Task::class], version = 1, exportSchema = true)
-abstract class BarnamehRizitoDatabase {
+abstract class BarnamehRizitoDatabase : RoomDatabase() {
     abstract fun barnamehRizitoDao(): BarnamehRizitoDao
 }
