da0a2e26fd41aba9c84f9388f86fbdaa1711d2ad
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-04-10 21:58:26 +0330

    Add and config toolbar

--- app/src/main/java/com/omidrezabagheriyan/barnamehrizitomobile/ui/MainActivity.kt
+++ app/src/main/java/com/omidrezabagheriyan/barnamehrizitomobile/ui/MainActivity.kt
@@ -2,6 +2,10 @@ package com.omidrezabagheriyan.barnamehrizitomobile.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.omidrezabagheriyan.barnamehrizitomobile.R
 import com.omidrezabagheriyan.barnamehrizitomobile.databinding.ActivityMainBinding
 import com.omidrezabagheriyan.barnamehrizitomobile.utils.viewbinding.viewBinding
@@ -10,9 +14,16 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityMainBinding::inflate)
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setup()
+    }
+
+    private fun setup(){
+
     }
 }
